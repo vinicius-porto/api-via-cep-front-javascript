586574d6a6fb34968dec8522f9d1a1fc0b78ed86
vinicius_porto <vinisepo@hotmail.com>
2025-08-06 15:43:37 -0300

    feat: remove alert padrão e cria função imprimirDados para exibir dados da API

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,6 +10,15 @@ function modalAlerta()
 });
 }
 
+function imprimirDados(data){
+     
+        Swal.fire({
+        text: data,
+        imageUrl: 'assets/img/Logo/Logo.svg',
+        imageWidth: 90,
+        confirmButtonText: 'Voltar'
+      });
+}
 
 function validarEnviar()
  {
@@ -23,8 +32,9 @@ const cep = document.getElementById("cep").value;
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
      .then(response => response.text())
         .then(data => {
-          
-          alert(data);
+       
+          imprimirDados(data);
+        
         })
         .catch(error => {
           console.error('Erro: 404', error);
